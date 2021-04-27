51a937fb6f130ea3cea12e3e73a9400290834112
azawrotniak <azawrotniak@gmail.com>
2021-04-27 20:57:11 +0200

    First commit

---
+++ appTechnical_Documentation_Management/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for appTechnical_Documentation_Management project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appTechnical_Documentation_Management.settings')
+
+application = get_wsgi_application()
